560a87fd1c33c90835f01f1a457221847b5eac13
bilal7317 <muhammadbilal7317@gmail.com>
2024-02-20 09:59:52 +0500

    First Commit

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DOTFILES_DIR=~/.dotfiles
+
+ln -sf $DOTFILES_DIR/.bashrc ~/.bashrc
+ln -sf $DOTFILES_DIR/.tmux.conf ~/.bashrc.conf
+ln -sf $DOTFILES_DIR/.vimrc ~/.vimrc
+# Add more lines for other dotfiles
+
+echo "Dotfiles installation complete"
+
